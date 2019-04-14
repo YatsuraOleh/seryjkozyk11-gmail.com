9f70d9c95f31ebf09c570002b541c31f82c11b5d
Oleh Yatsura <olehyatsura@gmail.com>
2019-04-14 20:26:24 +0300

    component registration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AddExpenseComponent } from './components/add-expense/add-expense.component';
+import { ExpensesListComponent } from './components/expenses-list/expenses-list.component';
+import { ExpenseItemComponent } from './components/expenses-list/expense-item/expense-item.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AddExpenseComponent,
+    ExpensesListComponent,
+    ExpenseItemComponent
+    
+    
   ],
   imports: [
     BrowserModule,
