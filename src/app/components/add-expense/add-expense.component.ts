14ca4c2819a1a8cb6a78ae4acb1c7542281148a2
Oleh Yatsura <olehyatsura@gmail.com>
2019-04-23 00:20:55 +0300

    ngModel

--- src/app/components/add-expense/add-expense.component.ts
+++ src/app/components/add-expense/add-expense.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-expense.component.css']
 })
 export class AddExpenseComponent implements OnInit {
+  public expense: string;
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
+  public onSubmit(){
+    console.log(new NewExpense(this.expense));
+    // this.expense.push(new NewExpense(this.expense));
+  }
 }
+class NewExpense {
+  constructor (
+    public expense: string,
+    public done: boolean = false,
+  ){}
+}
+
