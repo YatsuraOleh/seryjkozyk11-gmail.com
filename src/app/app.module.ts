850d1a55b6ee3d8f375a3646552115d9fec47ede
Oleh Yatsura <olehyatsura@gmail.com>
2019-04-23 00:13:13 +0300

    Add FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -24,6 +24,7 @@ import { AlertModule } from 'ngx-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     HttpClientModule,
     AlertModule.forRoot()
 
