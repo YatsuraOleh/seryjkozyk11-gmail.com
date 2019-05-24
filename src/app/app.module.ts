3b1e263eb204e3841d9f7e686cc5606ccfc28498
Oleh Yatsura <olehyatsura@gmail.com>
2019-05-24 09:53:27 +0300

    addition footerNew

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,21 @@ import { FormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { AddExpenseComponent } from './components/add-expense/add-expense.component';
 import { ExpensesListComponent } from './components/expenses-list/expenses-list.component';
 import { ExpenseItemComponent } from './components/expenses-list/expense-item/expense-item.component';
 import { AlertModule } from 'ngx-bootstrap';
+import { FooterNewComponent } from './components/footer-new/footer-new.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
     AddExpenseComponent,
     ExpensesListComponent,
-    ExpenseItemComponent
+    ExpenseItemComponent,
+    FooterNewComponent
   ],
   imports: [
     BrowserModule,
